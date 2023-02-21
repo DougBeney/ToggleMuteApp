631512d0703109aeff636f2f691417c812c37f8f
Dougie Beney <git@dougie.io>
2023-02-20 19:40:25 -0500

    comments

--- toggle-mute-spotify.py
+++ toggle-mute-spotify.py
@@ -4,19 +4,19 @@ import subprocess
 import re
 import sys
 
-# Define the command to run
-cmd = ['pactl', 'list', 'sink-inputs']
-
 # Run the command and capture its output as a byte string
-output_bytes = subprocess.check_output(cmd)
+output_bytes = subprocess.check_output(
+	['pactl', 'list', 'sink-inputs']
+)
 
 # Convert the byte string to a regular string using UTF-8 encoding
-output_str = output_bytes.decode('utf-8')
+sinks_str = output_bytes.decode('utf-8')
 
 sinks = {}
 
-# Print the output
-for s in output_str.split('\n\n'):
+# Loop through sinks string and convert entries to simple dict
+# to look up a sink by its application name.
+for s in sinks_str.split('\n\n'):
 	app_re = re.search(r'application\.name\s?\=\s?([\"\'\w\d\ ]+)', s)
 	id_re = re.search(r'Sink Input #([\w\ \d]+)', s)
 
@@ -25,9 +25,12 @@ for s in output_str.split('\n\n'):
 
 	sinks[app] = input_id
 
+# Use the first command line argument to toggle the mute of an app
 if len(sys.argv) > 1:
 	app = sys.argv[1]
 
 	if app in sinks:
-		cmd = subprocess.check_output(['pactl', 'set-sink-input-mute', sinks[app], 'toggle'])
+		cmd = subprocess.check_output(
+			['pactl', 'set-sink-input-mute', sinks[app], 'toggle']
+		)
 		print(cmd.decode('utf-8'))
